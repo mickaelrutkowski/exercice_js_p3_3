1ec8cafe766a8253aa356f5e080648151ebdbb56
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-10-11 18:20:53 +0200

    exercice p3 3

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function colorText() {
+  document.getElementById("text").style.color="green";
+}
+function colorText1() {
+  document.getElementById("text").style.color="red";
+}
+function colorText2() {
+  document.getElementById("text").style.color="blue";
+}
